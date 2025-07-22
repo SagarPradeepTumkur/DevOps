7062bdfed630edca86100f872f092abe08920022
SagarPradeepTumkur <113876118+SagarPradeepTumkur@users.noreply.github.com>
2025-07-22 23:11:21 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "DevOps2.pem" 
+  key_name      = "DevOps2" 
   tags = {
     Name = var.instance_name
   }
